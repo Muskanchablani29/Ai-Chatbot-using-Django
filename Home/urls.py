79491d16fef696d5dcc1461e3734fc9f1bcfcec3
MuskanChablani29 <chablanimuskan@gmail.com>
2025-04-16 18:39:42 +0530

    Added Homethree component

--- Home/urls.py
+++ Home/urls.py
@@ -5,8 +5,8 @@ app_name = 'Home'
 
 urlpatterns = [
     path('', views.Home, name='Home'),
-    path('Animationone/', views.Animationone, name='Animationone'),
-    path('Animationtwo/', views.Animationtwo, name='Animationtwo'),
-    path('Animationthree/', views.Animationthree, name='Animationthree'),
-    path('Animationfour/', views.Animationfour, name='Animationfour'),
+    path('animationone/', views.Animationone, name='Animationone'),
+    path('animationtwo/', views.Animationtwo, name='Animationtwo'),
+    path('animationthree/', views.Animationthree, name='Animationthree'),
+    path('animationfour/', views.Animationfour, name='Animationfour'),
 ]
